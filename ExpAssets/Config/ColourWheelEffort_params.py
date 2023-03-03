643858de840e0bdbac133a3f7c2f2b5b0329e3a2
Austin Hurst <mynameisaustinhurst@gmail.com>
2023-03-03 15:36:20 -0400

    Update params

--- ExpAssets/Config/ColourWheelEffort_params.py
+++ ExpAssets/Config/ColourWheelEffort_params.py
@@ -6,9 +6,9 @@
 collect_demographics = True
 manual_demographics_collection = False
 manual_trial_generation = False
-run_practice_blocks = False
+run_practice_blocks = True
 multi_user = False
-view_distance = 57 # in centimeters, 57cm = 1 deg of visual angle per cm of screen
+view_distance = 58 # in centimeters, 57cm = 1 deg of visual angle per cm of screen
 allow_hidpi = True
 
 #########################################
